0f8f3196d480ad29bb1f4e29284e4b22b12e9a69
Apeksha Kumawat <apeksha.kumawat@accolitedigital.com>
2024-04-09 15:09:07 +0530

    Final Commit

---
+++ AddFunctionality/AddFunctionality/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddFunctionality
+{
+    internal class Program
+    {
+        static double addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            double firstNo;
+            double secondNo;
+            double sum;
+
+            Console.WriteLine("Enter First Number : ");
+            firstNo = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            secondNo = Convert.ToDouble(Console.ReadLine());
+
+            sum = addition(firstNo, secondNo);
+            Console.WriteLine("Sum : " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
